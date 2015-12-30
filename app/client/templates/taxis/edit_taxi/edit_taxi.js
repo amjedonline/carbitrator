3daf6491f6ddae4f59ed8a5e77bfeb83a72c4b85
amjedonline <amjedonline@gmail.com>
2015-12-30 14:09:43 +0530

    Removing broken delete taxi functionality for now.

--- app/client/templates/taxis/edit_taxi/edit_taxi.js
+++ app/client/templates/taxis/edit_taxi/edit_taxi.js
@@ -8,15 +8,7 @@ Template.EditTaxi.events({
 /* EditTaxi: Helpers */
 /*****************************************************************************/
 Template.EditTaxi.helpers({
-    beforeRemove: function () {
-        return function (collection, id) {
-            var doc = collection.findOne(id);
-            if (confirm('Really delete car: "' + doc.brand + " " + doc.model + '"?')) {
-                this.remove();
-                Router.go('carsList');
-            }
-        };
-    }
+
 });
 
 /*****************************************************************************/
