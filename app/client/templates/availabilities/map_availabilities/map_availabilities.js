add71c2b09545e5d564eac44e1d18ce391642215
amjedonline <amjedonline@gmail.com>
2016-01-12 09:28:33 +0100

    issue-15: adding more map options.

--- app/client/templates/availabilities/map_availabilities/map_availabilities.js
+++ app/client/templates/availabilities/map_availabilities/map_availabilities.js
@@ -1,22 +1,32 @@
 /*****************************************************************************/
 /* MapAvailabilities: Event Handlers */
 /*****************************************************************************/
+
+Meteor.startup(function() {
+    GoogleMaps.load();
+});
+
+
 Template.MapAvailabilities.events({
 });
 
 /*****************************************************************************/
 /* MapAvailabilities: Helpers */
 /*****************************************************************************/
+
 Template.MapAvailabilities.helpers({
     availabilities: function () {
         return Availabilities.find({});
     },
-    myMapOptions: function() {
-        // Map initialization options
-        return {
-            center: {lat: -34.397, lng: 150.644},
-            zoom: 8
-        };
+    availabilitiesMapOptions: function() {
+        // Make sure the maps API has loaded
+        if (GoogleMaps.loaded()) {
+            // Map initialization options
+            return {
+                center: new google.maps.LatLng(-37.8136, 144.9631),
+                zoom: 13
+            };
+        }
     }
 });
 
@@ -27,40 +37,33 @@ Template.MapAvailabilities.onCreated(function () {
 });
 
 Template.MapAvailabilities.onRendered(function () {
+    GoogleMaps.load();
 });
 
 Template.MapAvailabilities.onDestroyed(function () {
 });
 
-
-/*
-GoogleMaps.ready('myMap', function(map) {
-    // Add a marker to the map once it's ready
-    navigator.geolocation.getCurrentPosition(function(location){
-        var position = { lat:location.coords.latitude,
-            lng:location.coords.longitude };
-        var marker = new google.maps.Marker({
-            position: position,
-            map: map.instance,
-            draggable: true,
-            animation: google.maps.Animation.DROP
+Template.body.onCreated(function(){
+    GoogleMaps.ready('availabilitiesMap', function(map) {
+        // Add a marker to the map once it's ready
+   /*     var marker = new google.maps.Marker({
+            position: map.options.center,
+            map: map.instance
+        });*/
+        var availabilities = availabilities.fetch();
+        _.each(availabilities, function(availability){
+            coords = availability.location.coordinates;
+            var position = {
+                lat:coordinates[0],
+                lng:coordinates[1]
+            };
+            console.log(position);
+            var provider = new google.maps.Marker({
+                position: position,
+                map: map.instance,
+                animation: google.maps.Animation.DROP
+            });
+            map.instance.setZoom(13);
         });
-        map.instance.setCenter(new google.maps.LatLng(
-            position.lat, position.lng));
-    });
-    map.instance.setZoom(15);
-});
-*/
-GoogleMaps.ready('myMap', function(map) {
-    console.log(" here is the providers");
-    var availabilities = availabilities.fetch();
-    _.each(availabilities, function(availability){
-        coords = availability.location.coordinates;
-        var position = { lat:coordinates[0],
-            lng:coordinates[1] };
-        console.log(position);
-        var provider = new google.maps.Marker({ position:position, map: map.instance, animation:google.maps.Animation.DROP});
-        console.log(provider)
-        map.instance.setZoom(13);
     });
 });
