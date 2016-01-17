a673b4696a3beba81cc5f011864569d2e2098a1d
amjedonline <amjedonline@gmail.com>
2016-01-17 22:26:40 +0100

    issue-15 adding simple typeahead autocomplete function.

--- app/client/startup.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by amjedonline on 17.01.16.
- */
-
-Meteor.typeahead.inject();
--- app/client/templates/availabilities/create_availability/create_availability.js
+++ app/client/templates/availabilities/create_availability/create_availability.js
@@ -1,14 +1,30 @@
+
 /*****************************************************************************/
 /* CreateAvailability: Event Handlers */
 /*****************************************************************************/
+/*
 Template.CreateAvailability.events({
+    'click #drivers': function(e) {
+        e.preventDefault();
+        $('#driversModal').modal('show');
+    }
 });
+*/
 
 /*****************************************************************************/
 /* CreateAvailability: Helpers */
 /*****************************************************************************/
 Template.CreateAvailability.helpers({
+    drivers:function(){
+        return Drivers.find().fetch().map(function(it){
+            return {value:it.fname};
+        }).slice(0, 100);
+    },
+    select: function(e){
+        console.log('selected');
+    }
 });
+
 
 /*****************************************************************************/
 /* CreateAvailability: Lifecycle Hooks */
@@ -17,7 +33,10 @@ Template.CreateAvailability.onCreated(function () {
 });
 
 Template.CreateAvailability.onRendered(function () {
+    Meteor.typeahead.inject();
+    console.log('Registered typeahead');
 });
 
 Template.CreateAvailability.onDestroyed(function () {
 });
+
